c99ca7b2484568c0b0245c5a88b627385e27bab6
Trevor Bernard <trevor.bernard@introhive.com>
2022-02-28 17:02:59 -0500

    Update lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,5 @@
-{:user {:plugins [[lein-ring "0.9.7"]
-                  [lein-ancient "0.6.14"]
-                  [lein-spell "0.1.0"]
-                  [codox "0.10.0"]
-                  [cider/cider-nrepl "0.15.1"]
-                  [lein-kibit "0.1.2"]
-                  [jonase/eastwood "0.2.3"]
-                  [lein-clojars "0.9.1"]]
-        :dependencies [[compliment "0.3.1" :scope "runtime"]]
+{:user {:plugins [[cider/cider-nrepl "0.27.3"]
+                  [lein-ancient "1.0.0-RC3"]]
+        :dependencies [[compliment "0.3.12" :scope "runtime"]]
         :aliases {"ccheck" ^{:doc "Clean and check the source"}
                   ["do" ["clean"] ["check"]]}}}

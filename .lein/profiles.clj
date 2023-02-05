0d26d87a4ee7d0e8e7f95e92a9bad616cbf4466e
Trevor Bernard <trevor.bernard@gmail.com>
2023-02-05 16:02:31 -0500

    All of the goodies. Using tpm to manage plugins for tmux

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.29.0"]]
-        :dependencies [[compliment "0.3.12" :scope "runtime"]]
         :aliases {"ccheck" ^{:doc "Clean and check the source"}
                   ["do" ["clean"] ["check"]]}}}
